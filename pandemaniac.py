6cfec34ae2d8a47c2845b60392c8f299ff094223
matathias <warrick.david58@gmail.com>
2016-02-22 12:39:36 -0800

    Changed seed selection method.
    Now picks nodes adjacent to the highest degree nodes, instead of the highest degree nodes themselves.

--- pandemaniac.py
+++ pandemaniac.py
@@ -39,19 +39,55 @@ for node in nodes:
         G.add_edge(node, neighbor)
         
 # Determine what nodes to use as seeds
-# Currently this is done by simply picking the nodes with the highest degrees.
-# This algorithm should definitely be improved upon.
+# Currently this is done by finding the highest degree nodes, and then picking
+# the highest degree nodes that connect to those nodes
 degrees = G.degree()
 v = list(degrees.values())
 k = list(degrees.keys())
         
 seeds = []
-for i in range(0, numSeeds):
-    val = max(v)
-    key = k[v.index(val)]
-    seeds.append(key)
-    v.remove(val)
+i = 0
+while i < numSeeds and len(v) > 0:
+    # Get the current highest degree node
+    currMax = max(v)
+    key = k[v.index(currMax)]
+    v.remove(currMax)
     k.remove(key)
+    
+    # Get the nodes adjacent to the current highest degree node
+    adjNodes = list(nx.all_neighbors(G, key))
+    if len(adjNodes) <= numSeeds - i:
+        # If the number of adjacent nodes is less than the number of nodes we
+        # needs to satisfy the seed requirement, then just add them all to the
+        # list (as long as they are not already there)
+        for j in range(0, len(adjNodes)):
+            if adjNodes[j] not in seeds:
+                seeds.append(adjNodes[j])
+                i += 1
+    elif len(adjNodes) == 0:
+        # This shouldn't ever be the case, but if it is...
+        seeds.append(key)
+        i += 1
+    else:
+        # Find out how many more seeds we need and get that many of the highest
+        # degree adjacent nodes
+        numLeft = numSeeds - i
+        adjDegs = G.degree(adjNodes)
+        adjDegv = list(adjDegs.values())
+        adjDegk = list(adjDegs.keys())
+        for j in range(0, numLeft):
+            val = max(adjDegv)
+            key2 = adjDegk[adjDegv.index(val)]
+            if key2 not in seeds:
+                seeds.append(key2)
+                i += 1
+            adjDegv.remove(val)
+            adjDegk.remove(key2)
+
+# Post-loop check to see that we have enough seed nodes
+if i < numSeeds:
+    print "Did not find enough seed nodes!"
+    exit()
     
 ### Write the chosen seeds to an output file
 # Get the name of the outfile from the infile
